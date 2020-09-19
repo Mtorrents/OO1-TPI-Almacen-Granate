bee7808df8ff6bdf1b3db8b45d3ce5f58be12dd4
Mtorrents <martin.torrents35@gmail.com>
2020-09-18 21:16:09 -0300

    subo tp OO1

---
+++ TPI Almacen Granate/src/modelo/Carrito.java
@@ -0,0 +1,93 @@
+package modelo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrito {
+	private int id;
+	private LocalDate fecha;
+	private LocalTime hora;
+	private boolean cerrado;
+	private double descuento;
+	private Cliente cliente;
+	private List<ItemCarrito> lstItemCarrito = new ArrayList<ItemCarrito>();
+	private Entrega entrega;
+
+	public Carrito(int id, LocalDate fecha, LocalTime hora, boolean cerrado, double descuento, Cliente cliente,
+			Entrega entrega) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.hora = hora;
+		this.cerrado = cerrado;
+		this.descuento = descuento;
+		this.cliente = cliente;
+		this.entrega = entrega;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public LocalTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+
+	public boolean isCerrado() {
+		return cerrado;
+	}
+
+	public void setCerrado(boolean cerrado) {
+		this.cerrado = cerrado;
+	}
+
+	public double getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<ItemCarrito> getLstItemCarrito() {
+		return lstItemCarrito;
+	}
+
+	public void setLstItemCarrito(List<ItemCarrito> lstItemCarrito) {
+		this.lstItemCarrito = lstItemCarrito;
+	}
+
+	public Entrega getEntrega() {
+		return entrega;
+	}
+
+	public void setEntrega(Entrega entrega) {
+		this.entrega = entrega;
+	}
+}
---
+++ TPI Almacen Granate/src/modelo/Comercio.java
@@ -0,0 +1,135 @@
+package modelo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comercio extends Actor {
+	private String nombreComercio;
+	private long cuit;
+	private double costoFijo;
+	private double costoPorKm;
+	private int diaDescuento;
+	private int porcentajeDescuentoDia;
+	private int porcentajeDescuentoEfectivo;
+	private List<DiaRetiro> lstDiaRetiro = new ArrayList<DiaRetiro>();
+	private List<Articulo> lstArticulo = new ArrayList<Articulo>();
+	private List<Carrito> lstCarrito = new ArrayList<Carrito>();
+
+	public Comercio(int id, Contacto contacto, String nombreComercio, long cuit, double costoFijo, double costoPorKm,
+			int diaDescuento, int porcentajeDescuentoDia, int porcentajeDescuentoEfectivo) {
+		super(id, contacto);
+		this.nombreComercio = nombreComercio;
+		this.cuit = cuit;
+		this.costoFijo = costoFijo;
+		this.costoPorKm = costoPorKm;
+		this.diaDescuento = diaDescuento;
+		this.porcentajeDescuentoDia = porcentajeDescuentoDia;
+		this.porcentajeDescuentoEfectivo = porcentajeDescuentoEfectivo;
+	}
+
+	public String getNombreComercio() {
+		return nombreComercio;
+	}
+
+	public void setNombreComercio(String nombreComercio) {
+		this.nombreComercio = nombreComercio;
+	}
+
+	public long getCuit() {
+		return cuit;
+	}
+
+	public void setCuit(long cuit) {
+		this.cuit = cuit;
+	}
+
+	public double getCostoFijo() {
+		return costoFijo;
+	}
+
+	public void setCostoFijo(double costoFijo) {
+		this.costoFijo = costoFijo;
+	}
+
+	public double getCostoPorKm() {
+		return costoPorKm;
+	}
+
+	public void setCostoPorKm(double costoPorKm) {
+		this.costoPorKm = costoPorKm;
+	}
+
+	public int getDiaDescuento() {
+		return diaDescuento;
+	}
+
+	public void setDiaDescuento(int diaDescuento) {
+		this.diaDescuento = diaDescuento;
+	}
+
+	public int getPorcentajeDescuentoDia() {
+		return porcentajeDescuentoDia;
+	}
+
+	public void setPorcentajeDescuentoDia(int porcentajeDescuentoDia) {
+		this.porcentajeDescuentoDia = porcentajeDescuentoDia;
+	}
+
+	public int getPorcentajeDescuentoEfectivo() {
+		return porcentajeDescuentoEfectivo;
+	}
+
+	public void setPorcentajeDescuentoEfectivo(int porcentajeDescuentoEfectivo) {
+		this.porcentajeDescuentoEfectivo = porcentajeDescuentoEfectivo;
+	}
+
+	public List<DiaRetiro> getLstDiaRetiro() {
+		return lstDiaRetiro;
+	}
+
+	public void setLstDiaRetiro(List<DiaRetiro> lstDiaRetiro) {
+		this.lstDiaRetiro = lstDiaRetiro;
+	}
+
+	public List<Articulo> getLstArticulo() {
+		return lstArticulo;
+	}
+
+	public void setLstArticulo(List<Articulo> lstArticulo) {
+		this.lstArticulo = lstArticulo;
+	}
+
+	public List<Carrito> getLstCarrito() {
+		return lstCarrito;
+	}
+
+	public void setLstCarrito(List<Carrito> lstCarrito) {
+		this.lstCarrito = lstCarrito;
+	}
+	
+	public LocalTime traerHoraRetiro(LocalDate fecha) {
+		DiaRetiro dr1 = null;
+		for(int i=0; i<lstDiaRetiro.size(); i++) {
+			if(lstDiaRetiro.get(i).getHoraHasta().atDate(fecha) != null) {
+				dr1 = lstDiaRetiro.get(i);
+			}
+		}
+		return dr1.getHoraHasta();
+	}
+	
+	public boolean agregarDiaRetiro(int diaSemana, LocalTime horaDesde, LocalTime horaHasta, int intervalo) {
+		lstDiaRetiro.add(new DiaRetiro(traerId(), diaSemana, horaDesde, horaHasta, intervalo));
+		return true;
+	}
+	
+	public int traerId() {
+		int idDiaRetiro = 1;
+		for(int i=0; i<lstDiaRetiro.size(); i++) {
+			idDiaRetiro++;
+		}
+		return idDiaRetiro;
+	}
+
+}
